406fd4e28469a5078c608b3d8a1456fd4340da80
Damon <dmarcr1997@gmail.com>
2021-08-06 23:19:56 -0400

    COM-4 Added conditions for variable setting for vol and bright

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const SerialPort = require('serialport');
-
+const open = require('open');
 const Readline = require('@serialport/parser-readline')
 const parser = new Readline()
 const port = new SerialPort("\\\\.\\COM7", {
@@ -11,7 +11,22 @@ parser.on('data', (data) => {
     if(datValue === "Close"){
         console.log("Goodbye ;)")
         process.exit()
-    } 
+    } else if(datValue === "New Tab"){
+        openChrome().then(() => console.log('/nOpened Tab/n')).catch(err => console.log(err))
+    } else if (datValue === "Left Locked") {
+        console.log("Increase Vol less 10/nDecrease Vol Greater than 20/nEnd Lock > 40")
+    } else if (datValue === "Volume Increased"){
+
+    } else if (datValue === "Volume Decreased"){
+
+    } else if (datValue === "Right Locked") {
+        console.log("Increase Brightness less 10/nDecrease Vol Greater than 20/nEnd Lock > 40")
+    } else if (datValue === "Brightness Increased"){
+
+    } else if (datValue === "Brightness Decreased"){
+
+    }
+    
     console.log(data)
 })
 
@@ -19,3 +34,8 @@ parser.on('data', (data) => {
 port.on('error', function(err) {
     console.log('Error: ', err.message)
 })
+
+async function openChrome(){
+    await open('https://google.com', {app: ['google chrome']});
+    
+}
