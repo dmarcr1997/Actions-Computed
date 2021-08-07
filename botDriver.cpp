4a745ff8d56c61825e56f4ae9accde106a4cd387
Damon <dmarcr1997@gmail.com>
2021-08-06 22:18:23 -0400

    COM-2 created base arduino code to process hand gestures

---
+++ botDriver.cpp
@@ -0,0 +1,103 @@
+int trigger1 = 8;      //Define IO pins
+int echo1 = 9;
+int trigger2 = 7;
+int echo2 = 6;
+
+long duration;
+int distL, distR;
+
+void setup()
+{
+  pinMode(trigger1, OUTPUT);   //Define pin
+  pinMode(echo1, INPUT);
+  pinMode(trigger2, OUTPUT);   //Define pin
+  pinMode(echo2, INPUT);
+  
+  Serial.begin(9600);           //Starts the serial communication
+}
+
+int calculateDist(int echo, int trig){
+  int distance = 0;
+  digitalWrite(trig, LOW);  
+  delay(1000);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH); 
+  distance= duration*0.034/2; 
+  if(distance > 60){
+    distance = 60;       
+  }
+  return distance;
+}
+
+void checkDists(int distL, int distR){
+  if ((distL >40 && distR>40) && (distL <60 && distR<60))
+  {
+    Serial.println("New Tab"); 
+    Serial.print("DistanceL: ");       
+    Serial.println(distL);
+    Serial.print("DistanceR: ");       
+    Serial.println(distR);
+    delay(500);
+  }
+  
+  if (distL>=10 && distL<=20)
+  {
+    Serial.println("Left Locked");
+    while(distL<=40)
+    {
+      
+      distL =calculateDist(echo1, trigger1);
+      Serial.print("DistanceL: ");       
+      Serial.println(distL);
+      if (distL<10) //Hand pushed in 
+      {
+        Serial.println("Volume Increased"); 
+        delay(300);
+      }
+      if (distL>20) //Hand pulled out
+      {
+        Serial.println("Volume Decreased"); 
+        delay(300);
+      }
+    }
+  }
+  
+  if (distR>=10 && distR<=20)
+  {
+    while(distR<=40)
+    {
+      distR =calculateDist(echo2, trigger2);
+      Serial.print("DistanceR: ");       
+      Serial.println(distR);
+      if (distR<10) 
+      {
+        Serial.println("Brightness Increased"); 
+        delay(300);
+      }
+      if (distR>20) 
+      {
+        Serial.println("Brightness Decreased"); 
+        delay(300);
+      }
+    } 
+  }
+  delay(200);
+}
+
+void loop()
+{
+  distL= calculateDist(echo1, trigger1);
+
+  distR= calculateDist(echo2, trigger2);
+
+  Serial.println("...");
+  Serial.print("DistanceL: ");
+  Serial.println(distL);
+  Serial.print("DistanceR: ");
+  Serial.println(distR);
+  Serial.println("...");
+  
+  checkDists(distL, distR);
+}
